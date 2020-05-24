393260f7c2c96ebd6be189d9800ec0cf653906f0
Jamin13P <benjaminthomsen1215@yahoo.com>
2020-05-24 11:35:44 -0700

    Getting close to finishing

--- src/components/Chooser.js
+++ src/components/Chooser.js
@@ -9,11 +9,14 @@ export default class Chooser extends Component {
     };
   }
 
-  checkChooser() {
-      
+  clickChooser() {
+    const {data} = this.props
+      this.props.trackPlayer(data.name, data.maxRank, data.maxMMR, data.matches, data.wins, data.losses, data.abandons, data.winPercent)
   }
 
   render() {
-    return <div>{this.props.data.name}</div>;
+    return <div>
+      <p onClick={() => this.clickChooser()}>{this.props.data.name}</p>
+      </div>;
   }
 }
