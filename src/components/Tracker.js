5be4df70f3f4b78f5a3fa87d784dde5eefd63ae0
Jamin13P <benjaminthomsen1215@yahoo.com>
2020-05-25 11:07:35 -0700

    finished

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -13,7 +13,6 @@ function Tracker(props) {
   return (
     <div className="tracker" style={{ fontSize: 20 }}>
       <h1>Tracker</h1>
-      <button>Add New Player</button>
       <div className="table">
         <section className="control">
           <div className="control-name">
